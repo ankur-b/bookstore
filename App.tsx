412c5b2ab3b3e96d6eb0f4a3635756be7b23f993
ankur-b <ankurbarve29@gmail.com>
2026-03-04 19:01:30 +0530

    Get Request & Flatlist

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, FlatList ,Image} from "react-native";
 
 export default function App() {
+  const [bookList, setBookList] = useState([]);
+
+  const endpointURL = "https://69a82b1c37caab4b8c60f5b8.mockapi.io/books";
+
+  const getListOfBooks = async () => {
+    const response = await axios.get(endpointURL);
+
+    setBookList(response.data);
+  };
+
+  console.log(bookList);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World.</Text>
       <StatusBar style="auto" />
+      <Button onPress={getListOfBooks} title="Get list of books" />
+      <FlatList
+        data={bookList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name_of_author}</Text>
+            <Text>{item.price}</Text>
+            <Image 
+              style={{height:150,width:150}}
+            source={{uri:item.cover}} />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
